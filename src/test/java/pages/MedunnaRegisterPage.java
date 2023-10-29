9233bc06bce93c22fbeafab090686744b39d8ddc
meliksahin05 <meliksahin05@gmail.com>
2023-10-30 00:59:52 +0200

    MedunnaRegister

---
+++ src/test/java/pages/MedunnaRegisterPage.java
@@ -0,0 +1,38 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class MedunnaRegisterPage {
+
+    public MedunnaRegisterPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(id = "ssn")
+    public WebElement ssn;
+
+    @FindBy(id = "firstName")
+    public WebElement firstName;
+
+    @FindBy(id = "lastName")
+    public WebElement lastName;
+
+    @FindBy(xpath = "//div[.='Your SSN is invalid']")
+    public WebElement invalidSsn;
+
+    @FindBy(xpath = "//div[.='Your SSN is required.']")
+    public WebElement requiredSsn;
+
+    @FindBy(id = "register-submit")
+    public WebElement registerSubmit;
+
+    @FindBy(xpath = "//div[.='Your FirstName is required.']")
+    public WebElement requiredFirstName;
+
+    @FindBy(xpath = "//div[.='Your LastName is required.']")
+    public WebElement requiredLastName;
+
+}
---
+++ src/test/java/utilities/Driver.java
@@ -0,0 +1,28 @@
+package utilities;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.time.Duration;
+
+public class Driver {
+
+    static WebDriver driver;
+
+    public static WebDriver getDriver(){
+
+        if (driver == null){
+            driver = new FirefoxDriver();
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+            driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(40));
+        }
+
+        return driver;
+
+    }
+
+
+}
